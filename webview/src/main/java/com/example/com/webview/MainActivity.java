584789f1def856a89901f572ac8567ca623da954
hj12190 <hj12190@hanmail.net>
2015-10-29 12:02:37 +0900

    webview

---
+++ webview/src/main/java/com/example/com/webview/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.com.webview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+    EditText edtUrl;
+    Button btnGo, btnBack;
+    WebView web;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        edtUrl = (EditText) findViewById(R.id.edtUrl);
+        btnGo = (Button) findViewById(R.id.btnGo);
+        btnBack = (Button) findViewById(R.id.btnBack);
+        web = (WebView) findViewById(R.id.webView1);
+
+       web.setWebViewClient(new TestWebViewClient());
+
+        WebSettings webSet = web.getSettings();
+        webSet.setBuiltInZoomControls(true);
+
+        btnGo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String http = "http://";
+                if(edtUrl.getText().toString().matches(http))
+                    web.loadUrl(edtUrl.getText().toString());
+                else
+                {
+                    web.loadUrl("http://" + edtUrl.getText().toString());
+                    edtUrl.setText("http://" + edtUrl.getText().toString());
+
+                }
+            }
+        });
+
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                web.goBack();
+            }
+        });
+    }
+
+    class TestWebViewClient extends WebViewClient {
+
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            edtUrl.setText(url);
+
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            return false;
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
